c12b1ae40c1e6d8f14e4a612fbe656aacd05d66d
Alexander Borovsky <alexander.borovsky@altoros.com>
2013-10-24 14:26:15 +0300

    Extracted MongoDB configuration logic to separate class

--- src/mongodb_controller/lib/mongodb_controller/mongo_cluster_builder.rb
+++ src/mongodb_controller/lib/mongodb_controller/mongo_cluster_builder.rb
@@ -1,6 +1,6 @@
 require 'socket'
 require 'uri'
-require 'mongo'
+require_relative 'mongodb_driver'
 
 module VCAP::MongodbController
   module MongoClusterBuilder
@@ -10,59 +10,40 @@ module VCAP::MongodbController
       def configure(config, message_bus)
         @config = config
         @message_bus = message_bus
+        @mongodb_driver = MongodbDriver.new(config)
       end
 
       def run
-        message_bus.subscribe("mongodb.advertise") do |msg, reply|
-          process_advertise_message(msg, reply)
+        if main_node?
+          message_bus.subscribe("mongodb.advertise") do |msg, reply|
+            process_advertise_message(msg, reply)
+          end
+        else
+          send_current_state
         end
-
-        send_current_state
       end
 
       def process_advertise_message(msg, reply)
-        p [:got_message, msg]
         unless repl_set_status
-          @mongo_session['admin'].command(replSetInitiate: 1) rescue nil
+          @mongo_driver.init_replication
         end
 
-        if(repl_set_master?)
+        if(@mongo_driver.replication_set.master?)
           puts "Adding member to cluster: #{msg.inspect}"
-          repl_set_add_member(msg["ip"])
+          @mongo_driver.replication_set.add_member(msg["ip"])
         end
       rescue
         puts $!, $!.backtrace
       end
 
       private
-
-      def repl_set_status
-        @mongo_session['local']['system.replset'].find_one
-      end
-
-      def repl_set_add_member(host)
-        set = Hash[repl_set_status.to_a]
-        p [:prevConfig, set]
-        max_version = set['members'].map{|h| h["_id"]}.max
-        set['members'] << { _id: max_version + 1, host: host }
-        set['version'] += 1
-        
-        p [:newConfig, set]
-        @mongo_session['admin'].command(replSetReconfig: set)
-      end
-
-      def repl_set_master?
-        @mongo_session['admin'].command(isMaster: 1)
-      end
-
       def send_current_state
         @mongo_session = ::Mongo::MongoClient.new('127.0.0.1', 27017)
 
         state = repl_set_status
-        p [:state, state]
-#        unless state
+        unless state
           message_bus.publish("mongodb.advertise", ip: local_ip)
-#        end
+        end
       end
 
       def local_ip
